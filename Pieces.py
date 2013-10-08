7316bb9967dbc23cfc4c412c58d46fdfb263d242
Tom Ritchford <tom@swirly.com>
2013-10-08 19:10:25 -0400

    Now we can print boards out with given moves.

--- Pieces.py
+++ Pieces.py
@@ -160,3 +160,55 @@ KNIGHT = [
   [0, 0, 0, 0, 0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0],
 ]
 
+KING = [
+  [0, 0, 0, 0, 0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1,  1,  1, 0, 0, 0, 0, 0, 0],
+
+  [0, 0, 0, 0, 1, 0, 1,  0,  1, 1, 0, 0, 0, 0, 0],
+
+  [0, 0, 0, 0, 0, 0, 1,  1,  1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0],
+]
+
+BOARD_OFFSET = 7
+
+def allowed_move(begin, end, piece):
+  x = end[0] - begin[0] + BOARD_OFFSET
+  y = end[1] - begin[1] + BOARD_OFFSET
+  return piece[y][x]
+
+def piece_move(begin, piece):
+  """List of coords we could move to if there were no other pieces on the
+  board."""
+
+  for y in range(8):
+    for x in range(8):
+      end = x, y
+      if allowed_move(begin, end, piece):
+        yield end
+
+def moves_on_board(moves):
+  board = [[0] * 8 for i in range(8)]
+
+  for x, y in moves:
+    board[y][x] = 1
+
+  for row in board:
+    for entry in row:
+      print('X' if entry else '.', end='')
+    print()
+
+def board_piece_moves(begin, piece):
+  moves_on_board(piece_move(begin, piece))
+
+
